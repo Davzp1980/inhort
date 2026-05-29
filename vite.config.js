aba3eb056e97a204ca7e5eac99baf2445935b29f
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2026-05-29 16:19:04 +0300

    seo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
+import sitemap from 'vite-plugin-sitemap';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    sitemap({
+      hostname: 'https://www.inhort.com.ua',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
